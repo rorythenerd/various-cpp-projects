665e16ea51e24d4c2628862f1fe77599830f03c9
G B <139829591+rorythenerd@users.noreply.github.com>
2025-03-11 02:43:05 -0400

    Update name.cpp

--- name.cpp
+++ name.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
 
-int main() {
-    std::string fullname = "Gracie Bao";
-    std::string firstname = fullname.substr(0, 6);
-    std::string lastname = fullname.substr(7, 10);
-    std::cout << "The full name is: " << fullname << '\n';
-    std::cout << "The first name is: " << firstname << '\n';
-    std::cout << "The last name is: " << lastname << '\n';
-}
